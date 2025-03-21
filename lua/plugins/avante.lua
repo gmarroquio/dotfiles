d031065cdb9c16fdec2344d39fde0b7109e63773
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-03-21 12:32:24 -0300

    change avante to gemini

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,7 +5,7 @@ return {
 	opts = {
 		-- add any opts here
 		-- for example
-		provider = "openai",
+		provider = "gemini",
 		openai = {
 			endpoint = "https://api.openai.com/v1",
 			model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
@@ -14,10 +14,15 @@ return {
 			max_tokens = 4096,
 			-- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
 		},
+		gemini = {
+			endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+			model = "gemini-2.0-flash",
+			timeout = 30000, -- Timeout in milliseconds
+			temperature = 0,
+			max_tokens = 20480,
+		},
 	},
-	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
-	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"stevearc/dressing.nvim",
@@ -29,7 +34,6 @@ return {
 		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
 		"ibhagwan/fzf-lua", -- for file_selector provider fzf
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-		"zbirenbaum/copilot.lua", -- for providers='copilot'
 		{
 			-- support for image pasting
 			"HakonHarnes/img-clip.nvim",
