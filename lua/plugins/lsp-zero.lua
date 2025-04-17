1f5c511493654bb3778ecb7a32e0d28557d85c10
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-04-17 09:59:59 -0300

    add snippets

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -80,6 +80,24 @@ return {
 		},
 		opts_extend = { "sources.default" },
 	},
+	-- snippets
+	{
+		"chrisgrieser/nvim-scissors",
+		dependencies = "nvim-telescope/telescope.nvim", -- if using telescope
+		opts = {
+			snippetDir = vim.fn.stdpath("config") .. "/snippets",
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>se", function()
+				require("scissors").editSnippet()
+			end, { desc = "Snippet: Edit" })
+
+			-- when used in visual mode, prefills the selection as snippet body
+			vim.keymap.set({ "n", "x" }, "<leader>sa", function()
+				require("scissors").addNewSnippet()
+			end, { desc = "Snippet: Add" })
+		end,
+	},
 
 	-- LSP
 	{
